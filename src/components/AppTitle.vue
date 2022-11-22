755f5563e6aa1a61efdb746215b26845e3431d50
AndreaDeCeglia <deceglia89@gmail.com>
2022-11-22 15:31:59 +0100

    Basic Project done

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>
+    My first Vue CLI App !!!!!!!
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'AppTitle',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
